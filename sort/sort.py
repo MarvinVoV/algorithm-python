938b4f7e24e719e35b56b9dceb8f58253c9ce8c1
MarvinVoV <marvin_vov@outlook.com>
2017-06-10 15:04:28 +0800

    selection sort

--- sort/sort.py
+++ sort/sort.py
@@ -2,7 +2,7 @@ def insertion_sort(a):
     """
     Insertion sort
     :param a: list
-    :return: void
+    :return: list
     """
     if not a or len(a) <= 1:
         return
@@ -13,8 +13,28 @@ def insertion_sort(a):
             a[i + 1] = a[i]
             i -= 1
         a[i + 1] = key
+    return a
+
+
+def selection_sort(a):
+    """
+    Selection sort
+    :param a: list
+    :return: list
+    """
+    if not a or len(a) <= 1:
+        return
+    for i in range(len(a)):
+        p = i
+        for j in range(i + 1, len(a)):
+            if a[j] < a[p]:
+                p = j
+        temp = a[p]
+        a[p] = a[i]
+        a[i] = temp
+    return a
 
 
 arr = [5, 2, 4, 6, 1, 3]
-insertion_sort(arr)
+selection_sort(arr)
 print(arr)
